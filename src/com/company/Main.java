465d919043006414707dea0c00b4c3466c81a79e
mzotov <themisterread@gmail.com>
2020-03-30 23:05:09 +0400

    Hello world app

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "World";
+        System.out.println(hello + " " + world + "!");
+    }
+}
